566e2b2adbb06dfd3ac6bac607f52fddd990910c
Troy Kelly <troy@troykelly.com>
2021-10-23 22:42:50 +1100

    Update torrent.sh

--- ruTorrent/torrent.sh
+++ ruTorrent/torrent.sh
@@ -366,14 +366,14 @@ execute.throw = bash, -c, (cat,"builtin cd \"", (cfg.basedir), "\" ","&& mkdir -
 # ----------------------------------------------------------------------
 
 throttle.max_downloads.global.set    = 10
-throttle.max_uploads.global.set      = 10
-throttle.min_peers.normal.set          = 500
-throttle.max_peers.normal.set          = 1000
-throttle.min_peers.seed.set              = 500
-throttle.max_peers.seed.set              = 1000
-throttle.max_uploads.set                   = 1000
+throttle.max_uploads.global.set	     = 10
+throttle.min_peers.normal.set	       = 500
+throttle.max_peers.normal.set	       = 1000
+throttle.min_peers.seed.set	         = 500
+throttle.max_peers.seed.set	         = 1000
+throttle.max_uploads.set	           = 1000
 throttle.global_down.max_rate.set_kb = 0
-throttle.global_up.max_rate.set_kb       = 0
+throttle.global_up.max_rate.set_kb	 = 0
 trackers.numwant.set                 = 250
 
 # ----------------------------------------------------------------------
@@ -409,7 +409,7 @@ network.http.max_open.set        = 48
 network.send_buffer.size.set     = 4M
 network.receive_buffer.size.set  = 4M
 network.xmlrpc.size_limit.set    = 4M
-dht.mode.set                       = auto
+dht.mode.set	                   = auto
 dht.port.set                     = $DHTPORT
 protocol.pex.set                 = yes
 
@@ -495,73 +495,73 @@ if [ -f "$RUTORRENTCONFDIR/$USERNAME/config.php" ]; then
 fi
 cat<<EOF>"$RUTORRENTCONFDIR/$USERNAME/config.php"
 <?php
-        // configuration parameters for $USERNAME
+	// configuration parameters for $USERNAME
 
-        // for snoopy client
-        @define('HTTP_USER_AGENT', 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0', true);
-        @define('HTTP_TIME_OUT', 30, true);     // in seconds
-        @define('HTTP_USE_GZIP', true, true);
-        \$httpIP = null;                                // IP string. Or null for any.
+	// for snoopy client
+	@define('HTTP_USER_AGENT', 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0', true);
+	@define('HTTP_TIME_OUT', 30, true);	// in seconds
+	@define('HTTP_USE_GZIP', true, true);
+	\$httpIP = null;				// IP string. Or null for any.
 
-        @define('RPC_TIME_OUT', 5, true);       // in seconds
+	@define('RPC_TIME_OUT', 5, true);	// in seconds
 
-        @define('LOG_RPC_CALLS', false, true);
-        @define('LOG_RPC_FAULTS', true, true);
+	@define('LOG_RPC_CALLS', false, true);
+	@define('LOG_RPC_FAULTS', true, true);
 
-        // for php
-        @define('PHP_USE_GZIP', false, true);
-        @define('PHP_GZIP_LEVEL', 2, true);
+	// for php
+	@define('PHP_USE_GZIP', false, true);
+	@define('PHP_GZIP_LEVEL', 2, true);
 
-        \$schedule_rand = 10;                   // rand for schedulers start, +0..X seconds
+	\$schedule_rand = 10;			// rand for schedulers start, +0..X seconds
 
-        \$do_diagnostic = true;
-        \$log_file = '$USERDIR/log/rutorrent.log';              // path to log file (comment or leave blank to disable logging)
+	\$do_diagnostic = true;
+	\$log_file = '$USERDIR/log/rutorrent.log';		// path to log file (comment or leave blank to disable logging)
 
-        \$saveUploadedTorrents = true;          // Save uploaded torrents to profile/torrents directory or not
-        \$overwriteUploadedTorrents = false;     // Overwrite existing uploaded torrents in profile/torrents directory or make unique name
+	\$saveUploadedTorrents = true;		// Save uploaded torrents to profile/torrents directory or not
+	\$overwriteUploadedTorrents = false;     // Overwrite existing uploaded torrents in profile/torrents directory or make unique name
 
-        \$topDirectory = '$USERDIR/';                   // Upper available directory. Absolute path with trail slash.
-        \$forbidUserSettings = false;
+	\$topDirectory = '$USERDIR/';			// Upper available directory. Absolute path with trail slash.
+	\$forbidUserSettings = false;
 
-        \$scgi_port = ${PORT};
-        \$scgi_host = "${HOSTNAME}";
+	\$scgi_port = ${PORT};
+	\$scgi_host = "${HOSTNAME}";
 
-        #\$scgi_port = 0;
-        #\$scgi_host = "unix://$RPCSOCKET";
+	#\$scgi_port = 0;
+	#\$scgi_host = "unix://$RPCSOCKET";
 
-        // For web->rtorrent link through unix domain socket
-        // (scgi_local in rtorrent conf file), change variables
-        // above to something like this:
-        //
-        // \$scgi_port = 0;
-        // \$scgi_host = "unix:///tmp/rpc.socket";
+	// For web->rtorrent link through unix domain socket
+	// (scgi_local in rtorrent conf file), change variables
+	// above to something like this:
+	//
+	// \$scgi_port = 0;
+	// \$scgi_host = "unix:///tmp/rpc.socket";
 
-        \$XMLRPCMountPoint = "/RPC$INSTANCE";           // DO NOT DELETE THIS LINE!!! DO NOT COMMENT THIS LINE!!!
+	\$XMLRPCMountPoint = "/RPC$INSTANCE";		// DO NOT DELETE THIS LINE!!! DO NOT COMMENT THIS LINE!!!
 
-        \$pathToExternals = array(
-                "php"   => '',                  // Something like /usr/bin/php. If empty, will be found in PATH.
-                "curl"  => '',                  // Something like /usr/bin/curl. If empty, will be found in PATH.
-                "gzip"  => '',                  // Something like /usr/bin/gzip. If empty, will be found in PATH.
-                "id"    => '',                  // Something like /usr/bin/id. If empty, will be found in PATH.
-                "stat"  => '',                  // Something like /usr/bin/stat. If empty, will be found in PATH.
-        );
+	\$pathToExternals = array(
+		"php" 	=> '',			// Something like /usr/bin/php. If empty, will be found in PATH.
+		"curl"	=> '',			// Something like /usr/bin/curl. If empty, will be found in PATH.
+		"gzip"	=> '',			// Something like /usr/bin/gzip. If empty, will be found in PATH.
+		"id"	=> '',			// Something like /usr/bin/id. If empty, will be found in PATH.
+		"stat"	=> '',			// Something like /usr/bin/stat. If empty, will be found in PATH.
+	);
 
-        \$localhosts = array(                   // list of local interfaces
-                "127.0.0.1",
-                "localhost",
-                "${HOSTNAME}"
-        );
+	\$localhosts = array( 			// list of local interfaces
+		"127.0.0.1",
+		"localhost",
+		"${HOSTNAME}"
+	);
 
-        \$profilePath = '${TORRENTSDIR}/share';         // Path to user profiles
-        \$profileMask = 0777;                   // Mask for files and directory creation in user profiles.
-                                                // Both Webserver and rtorrent users must have read-write access to it.
-                                                // For example, if Webserver and rtorrent users are in the same group then the value may be 0770.
+	\$profilePath = '${TORRENTSDIR}/share';		// Path to user profiles
+	\$profileMask = 0777;			// Mask for files and directory creation in user profiles.
+						// Both Webserver and rtorrent users must have read-write access to it.
+						// For example, if Webserver and rtorrent users are in the same group then the value may be 0770.
 
-        \$tempDirectory = '$USERDIR/tmp';                       // Temp directory. Absolute path with trail slash. If null, then autodetect will be used.
+	\$tempDirectory = '$USERDIR/tmp';			// Temp directory. Absolute path with trail slash. If null, then autodetect will be used.
 
-        \$canUseXSendFile = true;               // If true then use X-Sendfile feature if it exist
+	\$canUseXSendFile = true;		// If true then use X-Sendfile feature if it exist
 
-        \$locale = "UTF8";
+	\$locale = "UTF8";
 EOF
 
 if [ ! -f "$RUTORRENTCONFDIR/$USERNAME/access.ini" ]; then
@@ -609,7 +609,7 @@ if [ ! -f "$RUTORRENTCONFDIR/$USERNAME/plugins.ini" ]; then
 ;; For setting individual plugin permissions you must write something like that:
 ;;
 ;; [ratio]
-;; enabled = yes                        ;; also may be "user-defined", in this case user can control plugin's state from UI
+;; enabled = yes			;; also may be "user-defined", in this case user can control plugin's state from UI
 ;; canChangeToolbar = yes
 ;; canChangeMenu = yes
 ;; canChangeOptions = no
