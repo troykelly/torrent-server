36357e906080c9a9e311d7c3a95184ecf0421b44
Troy Kelly <troy@troykelly.com>
2022-03-12 12:40:48 +1100

    Fixing file fetch

--- ruWebfiles/getfiles.sh
+++ ruWebfiles/getfiles.sh
@@ -1,20 +1,68 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-cd /app
-git init
-git remote add origin ${RUREPO}
-git fetch origin
-git checkout -b master --track origin/master # origin/master is clone's default
-git reset origin/master
-chown -R ${APP_USER}:${APP_GROUP} /app
-chown -R ${APP_USER}:${APP_GROUP} /torrents
-chown -R ${APP_USER}:${APP_GROUP} /security
-mkdir -p /security/caddy
-touch /security/caddy/users
-find /app -type d -exec chmod 775 "{}" \;
-find /app -type f -exec chmod 664 "{}" \;
-find /app -type f -name "*.sh" -exec chmod 775 "{}" \;
-find /torrents -type d -exec chmod 775 "{}" \;
-find /torrents -type f -exec chmod 664 "{}" \;
-find /security -type d -exec chmod 775 "{}" \;
-find /security -type f -exec chmod 664 "{}" \;
+if [ -z "${RUTORRENT_APP_DIR}" ]; then
+    RUTORRENT_APP_DIR="/app"
+fi
+if [ -z "${TORRENTS_DIR}" ]; then
+    TORRENTS_DIR="/torrents"
+fi
+if [ -z "${SECURITY_DIR}" ]; then
+    SECURITY_DIR="/security"
+fi
+if [ -z "${RUREPO}" ]; then
+    RUREPO="https://github.com/Novik/ruTorrent.git"
+fi
+if [ -z "${APP_USER}" ]; then
+    APP_USER="$(whoami)"
+fi
+if [ -z "${APP_GROUP}" ]; then
+    APP_GROUP="$(id -g -n)"
+fi
+
+getRUTorrent ()
+{
+    echo "📁 Get ruTorrent"
+    mkdir -p "${RUTORRENT_APP_DIR}" && \
+        cd "${RUTORRENT_APP_DIR}" && \
+        git init && \
+        git remote add origin "${RUREPO}" && \
+        git fetch origin && \
+        git checkout -b master --track origin/master && \
+        git reset origin/master
+}
+
+updateRUTorrent ()
+{
+    echo "📂 Update ruTorrent"
+    cd "${RUTORRENT_APP_DIR}"
+    git fetch --all
+    git reset --hard origin/master
+}
+
+resetPermissions ()
+{
+    echo "🗄️ Scaffolding files"
+    mkdir -p "${SECURITY_DIR}/caddy"
+    mkdir -p "${TORRENTS_DIR}"
+    touch "${SECURITY_DIR}/caddy/users"
+
+    echo "🔒 Restting permissions to user ${APP_USER} and group ${APP_GROUP}"
+    chown -R ${APP_USER}:${APP_GROUP} "${RUTORRENT_APP_DIR}"
+    chown -R ${APP_USER}:${APP_GROUP} "${TORRENTS_DIR}"
+    chown -R ${APP_USER}:${APP_GROUP} "${SECURITY_DIR}"
+    find "${RUTORRENT_APP_DIR}" -type d -exec chmod 775 "{}" \;
+    find "${RUTORRENT_APP_DIR}" -type f -exec chmod 664 "{}" \;
+    find "${RUTORRENT_APP_DIR}" -type f -name "*.sh" -exec chmod 775 "{}" \;
+    find "${TORRENTS_DIR}" -type d -exec chmod 775 "{}" \;
+    find "${TORRENTS_DIR}" -type f -exec chmod 664 "{}" \;
+    find "${SECURITY_DIR}" -type d -exec chmod 775 "{}" \;
+    find "${SECURITY_DIR}" -type f -exec chmod 664 "{}" \;
+}
+
+if [ ! -f "${RUTORRENT_APP_DIR}/php/rtorrent.php" ]; then
+    getRUTorrent
+else
+    updateRUTorrent
+fi
+
+resetPermissions
